ca24ab4277b54fe0848aa3cc32ef9e7886ec4cce
Sarah Rose <156587765+SarahRoseLives@users.noreply.github.com>
2025-06-28 09:54:30 -0400

    Update config.dart

--- config.dart
+++ config.dart
@@ -1,12 +1,14 @@
+// lib/config.dart
+
 // Set this to true if you are running on an Android emulator
 // and your Go backend is running on your host machine.
 // '10.0.2.2' is a special alias for your host machine's localhost from within an Android emulator.
 const bool isAndroidEmulator = false; // <--- Set to true for Android Emulator/Device, false for Linux/Web/iOS Simulator
 
 // Define your API host and port
-const String _apiHostAddress = "transirc.chat"; // Update with your server IP or domain if needed
+const String _apiHostAddress = "iris.transirc.chat"; // Update with your server IP or domain if needed
 const String _apiHostEmulator = "10.0.2.2"; // Android emulator's loopback to host
-const int apiPort = 8080; // Make this constant accessible
+const int apiPort = 443;
 
 // Determine the active API host based on the flag
 String get apiHost {
@@ -15,10 +17,15 @@ String get apiHost {
 
 // Full base URL for HTTP API calls
 String get baseUrl {
-  return "http://$apiHost:$apiPort/api";
+  return "https://$apiHost:$apiPort/api";
+}
+
+// Full base URL for assets, like avatars and attachments
+String get baseSecureUrl {
+  return "https://$apiHost:$apiPort";
 }
 
 // Full base URL for WebSocket connections
 String get websocketUrl {
-  return "ws://$apiHost:$apiPort/ws";
+  return "wss://$apiHost:$apiPort/ws"; // Using WSS for secure WebSocket
 }
